933b1b5bee89ce20e1cfea3048e272896b19511f
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2023-08-19 12:06:56 +0200

    Add style

--- src/components/CVDocument.jsx
+++ src/components/CVDocument.jsx
@@ -1,22 +1,52 @@
 function CVDocument({ personalDetails, education, experience }) {
+	const cvStyle = {
+		width: "8.27in",
+		height: "11.69",
+		backgroundColor: "white",
+		color: "#213547",
+		fontSize: "12pt",
+	};
+	const headerStyle = {
+		color: "white",
+		backgroundColor: "#0c4a6e",
+		padding: "0.5in 1in",
+		textAlign: "center",
+	};
 	return (
-		<div className="cv-document">
-			<div className="cv-header">
-				<h1>{personalDetails.fullName}</h1>
-				<p className="contact-details">
-					<span>{personalDetails.email}</span>
-					<span>{personalDetails.phone}</span>
+		<div className="cv-document" style={cvStyle}>
+			<div className="cv-header" style={headerStyle}>
+				<h1 style={{ margin: "0" }}>{personalDetails.fullName}</h1>
+				<p
+					className="contact-details"
+					style={{
+						margin: "0.5em",
+						display: "flex",
+						justifyContent: "center",
+						fontWeight: 500,
+					}}
+				>
+					<span
+						style={{ borderRight: "1px solid white", paddingRight: "0.75rem" }}
+					>
+						{personalDetails.email}
+					</span>
+					<span style={{ paddingLeft: "1rem" }}>{personalDetails.phone}</span>
 				</p>
-				<p>{personalDetails.address}</p>
+				<p style={{ margin: "0.5em" }}>{personalDetails.address}</p>
 			</div>
-			<div className="cv-body">
+			<div
+				className="cv-body"
+				style={{
+					padding: "0.25in 1in 1in 1in",
+				}}
+			>
 				{education.length != 0 && (
 					<section className="education">
 						<h2>Education</h2>
 						{education.map((item) => {
 							return (
 								<div key={item.id}>
-									<div>
+									<div className="date">
 										<p>
 											<strong>
 												{item.startDate} - {item.endDate}
@@ -25,9 +55,11 @@ function CVDocument({ personalDetails, education, experience }) {
 									</div>
 									<div>
 										<p>
-											<strong>{item.school}</strong>
+											<strong>{item.degree}</strong>
 										</p>
-										<p>{item.degree}</p>
+										<p>
+											<em>{item.school}</em>
+										</p>
 									</div>
 								</div>
 							);
@@ -40,7 +72,7 @@ function CVDocument({ personalDetails, education, experience }) {
 						{experience.map((item) => {
 							return (
 								<div key={item.id}>
-									<div>
+									<div className="date">
 										<p>
 											<strong>
 												{item.startDate} - {item.endDate}
@@ -49,9 +81,8 @@ function CVDocument({ personalDetails, education, experience }) {
 									</div>
 									<div>
 										<p>
-											<strong>{item.company}</strong>
+											<strong>{item.position}</strong>, <em>{item.company}</em>
 										</p>
-										<p>{item.position}</p>
 										<p>{item.description}</p>
 									</div>
 								</div>
