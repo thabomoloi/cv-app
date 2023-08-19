cb31ae2229f618194e34975414befad5d86c2af3
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2023-08-19 08:52:43 +0200

    Add C document component

--- src/components/CVDocument.jsx
+++ src/components/CVDocument.jsx
@@ -1,3 +1,67 @@
-function CVDocument() {}
+function CVDocument({ personalDetails, education, experience }) {
+	return (
+		<div className="cv-document">
+			<div className="cv-header">
+				<h1>{personalDetails.fullName}</h1>
+				<p className="contact-details">
+					<span>{personalDetails.email}</span>
+					<span>{personalDetails.phone}</span>
+				</p>
+				<p>{personalDetails.address}</p>
+			</div>
+			<div className="cv-body">
+				{education.length != 0 && (
+					<section className="education">
+						<h2>Education</h2>
+						{education.map((item) => {
+							return (
+								<div key={item.id}>
+									<div>
+										<p>
+											<strong>
+												{item.startDate} - {item.endDate}
+											</strong>
+										</p>
+									</div>
+									<div>
+										<p>
+											<strong>{item.school}</strong>
+										</p>
+										<p>{item.degree}</p>
+									</div>
+								</div>
+							);
+						})}
+					</section>
+				)}
+				{experience.length != 0 && (
+					<section className="experience">
+						<h2>Experience</h2>
+						{experience.map((item) => {
+							return (
+								<div key={item.id}>
+									<div>
+										<p>
+											<strong>
+												{item.startDate} - {item.endDate}
+											</strong>
+										</p>
+									</div>
+									<div>
+										<p>
+											<strong>{item.company}</strong>
+										</p>
+										<p>{item.position}</p>
+										<p>{item.description}</p>
+									</div>
+								</div>
+							);
+						})}
+					</section>
+				)}
+			</div>
+		</div>
+	);
+}
 
 export default CVDocument;
